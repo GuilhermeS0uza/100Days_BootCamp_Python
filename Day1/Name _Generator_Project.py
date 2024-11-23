7575476724ef46c89c264d6b2ad1269d5f2812e7
GuilhermeS0uza <guibsouza04@gmail.com>
2024-11-22 21:50:38 -0300

    Name_Generator_Project

---
+++ Day1/Name _Generator_Project.py
@@ -0,0 +1,4 @@
+print("Bem-vindo ao gerador de nomes de marca.")
+cidade = input("Qual que é o nome da cidade que você nasceu?\n")
+animal = input("Qual que é o nome do seu animal de estimação?\n")
+print("O nome da sua marca poderia ser: " + cidade + " " + animal)
